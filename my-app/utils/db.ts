afd005a3af1c7a6444e19a0147850c4ec5aeedf4
shunsuke6 <sawabes@ns.aucnet.co.jp>
2024-02-01 03:21:31 +0900

    feat: Add db

--- my-app/utils/db.ts
+++ my-app/utils/db.ts
@@ -1,31 +1,28 @@
-import {Clinet} from "postgress";
+import { Clinet } from "postgresql";
 import "dotenv/load.ts";
 
-export interface Data{
-    id: string;
-    name:string;
+export interface Data {
+  id: string;
+  name: string;
 }
 
 const client = new Client({
-    user: Deno.env.get("DB_USER"),
-    database: Deno.env.get("DB_NAME"),
-    hostname: Deno.env.get("DB_HOST"),
-    password: Deno.env.get("DB_PASSWORD"),
-    port: Deno.env.get("DB_PORT"),
+  user: Deno.env.get("DB_USER"),
+  database: Deno.env.get("DB_NAME"),
+  hostname: Deno.env.get("DB_HOST"),
+  password: Deno.env.get("DB_PASSWORD"),
+  port: Deno.env.get("DB_PORT"),
 });
-
 
 await client.connect();
 
-export const SelectAll = async() =>{
-    try {
-        const result = await client.queryObject<Data>(
-            "SELECT * FROM data"
-        );
-        return result.rows;
-    }catch(ex){
-        console.error(ex);
-        return[];
-    }
-}
+export const SelectAll = async () => {
+  try {
+    const result = await client.queryObject<Data>("SELECT * FROM data");
 
+    return result.rows;
+  } catch (ex) {
+    console.error(ex);
+    return [];
+  }
+};
